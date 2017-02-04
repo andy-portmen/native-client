e8e36920a22b27662d70d39805eac84953f50ef4
Andy Portmen <andy.portmen@gmail.com>
2017-02-04 13:39:58 +0330

    supporting open in Chrome for Firefox and Opera browsers

--- config.js
+++ config.js
@@ -18,10 +18,12 @@ exports.ids = {
     'bifmfjgpgndemajpeeoiopbeilbaifdo', // External Application Button (Chrome)
     'enemdfoackoekaedijjmjlckkleokhih', // External Application Button (Opera)
     'ihpiinojhnfhpdmmacgmpoonphhimkaj', // Open in VLC (Chrome)
+    'gmdcejkkcimnbfmicdpohgicekomipde', // Open in Chrome (Opera)
   ],
   firefox: [
     '{5610edea-88c1-4370-b93d-86aa131971d1}', // Open in IE
     '{0ff128a1-c286-4e73-bffa-9ae879b244d5}', // Media Converter
     '{65b77238-bb05-470a-a445-ec0efe1d66c4}', // External Application Button
+    '{086f665e-6a55-4107-9147-f9a14e72b137}', // Open in Chrome
   ]
 };
