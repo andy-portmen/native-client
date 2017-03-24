5dfc84b52f11452ea3af4bbadc9c76319191349d
Andy Portmen <andy.portmen@gmail.com>
2017-03-24 16:27:52 +0430

    updating version number to 0.3.2

--- host.js
+++ host.js
@@ -20,7 +20,7 @@ var https = lazyRequire('./follow-redirects').https;
 var server, files = [];
 
 var config = {
-  version: '0.3.1'
+  version: '0.3.2'
 };
 // closing node when parent process is killed
 process.stdin.resume();
