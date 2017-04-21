45be3e387cb343cda950f4ee4d4090fc15f9a081
Andy Portmen <andy.portmen@gmail.com>
2017-04-21 11:44:23 +0430

    updating Linux NodeJS to v6.10.2 (64-bit)

--- host.js
+++ host.js
@@ -21,7 +21,7 @@ var https = lazyRequire('./follow-redirects').https;
 var server, files = [], sprocess = [];
 
 var config = {
-  version: '0.3.4'
+  version: '0.3.5'
 };
 // closing node when parent process is killed
 process.stdin.resume();
