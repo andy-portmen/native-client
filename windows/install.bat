793bc5260a11d452197c116fb57552bf4beccb10
Andy Portmen <andy.portmen@gmail.com>
2017-04-23 17:43:29 +0430

    removing admin permission for installing the native client on Windows (thanks to @InBasic comment)

--- windows/install.bat
+++ windows/install.bat
@@ -1,39 +1,16 @@
 @echo off
 
-call :isAdmin
-
-if %errorlevel% == 0 (
-    goto :run
-) else (
-    echo .
-    echo .
-    echo ************ERROR*****************
-    echo *******Run as administrator*******
-    echo **********************************
-    echo .
-    echo .
-    pause
-)
-
-exit /b
-
-:isAdmin
-fsutil dirty query %systemdrive% >nul
-exit /b
-
-:run
-
 echo .. Writting to Chrome Registry
 echo .. Key: HKCU\Software\Google\Chrome\NativeMessagingHosts\com.add0n.node
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.add0n.node" /ve /t REG_SZ /d "%ProgramFiles%\com.add0n.node\manifest-chrome.json" /f
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.add0n.node" /ve /t REG_SZ /d "%LocalAPPData%\com.add0n.node\manifest-chrome.json" /f
 
 echo .. Writting to Firefox Registry
-echo .. Key: HKEY_LOCAL_MACHINE\SOFTWARE\Mozilla\NativeMessagingHosts\com.add0n.node
-for %%f in ("%PROGRAMFILES%") do SET SHORT_PATH=%%~sf
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Mozilla\NativeMessagingHosts\com.add0n.node" /ve /t REG_SZ /d "%SHORT_PATH%\com.add0n.node\manifest-firefox.json" /f
+echo .. Key: HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\com.add0n.node
+for %%f in ("%LocalAPPData%") do SET SHORT_PATH=%%~sf
+REG ADD "HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\com.add0n.node" /ve /t REG_SZ /d "%SHORT_PATH%\com.add0n.node\manifest-firefox.json" /f
 
 pushd "%~dp0"
 cd app
-..\node.exe install.js "%ProgramFiles%"
+..\node.exe install.js "%LocalAPPData%"
 
 pause
--- windows/uninstall.bat
+++ windows/uninstall.bat
@@ -1,37 +1,14 @@
 @echo off
 
-call :isAdmin
-
-if %errorlevel% == 0 (
-    goto :run
-) else (
-    echo .
-    echo .
-    echo ************ERROR*****************
-    echo *******Run as administrator*******
-    echo **********************************
-    echo .
-    echo .
-    pause
-)
-
-exit /b
-
-:isAdmin
-fsutil dirty query %systemdrive% >nul
-exit /b
-
-:run
-
 echo .. Deleting Chrome Registry
 REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.add0n.node" /f
 
 echo .. Deleting Firefox Registry
-for %%f in ("%PROGRAMFILES%") do SET SHORT_PATH=%%~sf
-REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Mozilla\NativeMessagingHosts\com.add0n.node" /f
+for %%f in ("%LocalAPPData%") do SET SHORT_PATH=%%~sf
+REG DELETE "HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\com.add0n.node" /f
 
 echo .. Deleting com.add0n.node
-RMDIR /Q /S "%ProgramFiles%\com.add0n.node"
+RMDIR /Q /S "%LocalAPPData%\com.add0n.node"
 
 echo.
 echo ^>^>^> Done! ^<^<^<
