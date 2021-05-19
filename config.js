28d3895374d446a2ae555e53e3b6664750912214
Andy Portmen <andy.portmen@gmail.com>
2021-05-19 02:41:48 -0400

    remove unused ids

--- config.js
+++ config.js
@@ -3,67 +3,57 @@
 exports.ids = {
   chrome: [
     'lmeddoobegbaiopohmpmmobpnpjifpii', // Open in Firefox (Chrome)
-    'hhalgjmpmjelidhhjldondajffjbcmcg', // Open in Firefox (Opera)
-    'ajgodcbbfnpdbopgmfcgdbfhabbnilbp', // Open in Firefox (Edge)
-    'poibpkhpegdblnblbkcppknekhkhmmlp', // Open in Edge (Chrome)
     'mjoebkkejejidnkfdekpbooceogbapnf', // Open in Edge (Chrome) by @rynu.smith
-    'jjlkfjglmelpbkgfeebcdipalggpapag', // Open in Edge (Opera)
     'amojccmdnkdlcjcplmkijeenigbhfbpd', // Open in Opera (Chrome)
-    'eokgbaidifojhfknoiicmnffiaakelpm', // Open in Opera (Edge)
-    'eoenobhieedpimobhembbgdcdhdcdcig', // Open in Safari (Chrome)
-    'bdlaolidgmapdilglbocpmbfipdpfjef', // Open in Safari (Opera)
-    'ncemgeeikbbjoojolbihkpcoiomidhfi', // Open in IE (Edge)
-    'noedpljikmfalclpadmnjbabbignpfge', // Open in IE (Opera)
-    'agaecbnjediafcdopcfidcdiponjlmnk', // Open in IE (Chrome)
     'looohpideggedchhpphemdmppnmdkgfd', // Open in IE (Chrome) by @rynu.smith
     'ocnfecjfebnllnapjjoncgjnnkfmobjc', // Media Converter (Chrome) [By @InBasic]
-    'fabccabfpmdadbhljpcmcbmepepfllnb', // Media Converter (Opera) [By @InBasic]
     'jgpghknlbaljigdhcjimjnkkjniiipmm', // Open in GIMP (Chrome)
-    'cmjahocdpafkodabbojjaebogoigcipj', // Open in GIMP (Opera)
     'bifmfjgpgndemajpeeoiopbeilbaifdo', // External Application Button (Chrome)
-    'enemdfoackoekaedijjmjlckkleokhih', // External Application Button (Opera)
-    'icfhhpfimihpdgglmdlnmpaadfeaacbk', // External Application Button (Edge)
     'ihpiinojhnfhpdmmacgmpoonphhimkaj', // Open in VLC (Chrome)
-    'jfpmkcifglhenaihlnefaccjbmcmbigf', // Open in VLC (Opera)
-    'hjfcjapkfahlmlefedkkpbbkeddpnnlc', // Open in VLC (Edge)
-    'gmdcejkkcimnbfmicdpohgicekomipde', // Open in Chrome (Opera)
-    'hkjcboammhlecfomgabecgoppnncbhjp', // Open in Chrome (Edge)
     'gkmonffckeeffppchajngpdakfppalfo', // Auto Shutdown (Chrome)
-    'iajkcajifgglcaojccgiohphgjmoleia', // Auto Shutdown (Opera)
-    'klglmiipkjlhpeeopnnlkhdomjhbkpjh', // Open in Vivaldi (Chrome)
-    'ldoihpohemnbokndocafmiblepbjeacb', // Open in Vivaldi (Opera)
-    'dijcjcnibopfdgbmpkgnjfdihfdeghcc', // Open in Tor Browser (Chrome)
-    'emlgnifoahkaeocfmafkpnfbijoioefh', // Open in Tor Browser (Opera)
-    'baailbcimkljcoiocaodblapjgklhgdm', // Open in Pale Moon (Chrome)
-    'cglibgbhfapdcidmcpnbbekdanacjgkf', // Open in Pale Moon (Opera)
-    'eekmpanhabepggphkcpnkngfmifkffip', // Open in Waterfox (Chrome)
-    'hedcbilfmphgkpnokgcflaljgpecagfd', // Open in Waterfox (Opera)
     'kjoabfljeghcinlpjhdbdfbcflapkccm', // Tor Control (Chrome)
-    'knfbgpkbkfebddfbklfpgmdjgolnkkfl', // Tor Control (Opera)
-    'ijhhpgjhnfoalaedkmgpbehokefplfni', // Tor Control (Edge)
-    'khgbdhkpcapllhgfekjegcinegfhjbmi', // YVAD by (Opera) [By @InBasic]
     'cehiomcamjpnfmemkmpjadaclohoibgo', // Open in PDF viewer (Chrome)
-    'honagjpkinogkppkapphkpdffnegagge', // Open in PDF viewer (Opera)
-    'ciphgjdgpkhlngiadnpebblpcjcoabcp', // Open in PDF viewer (Edge)
     'nfpgfobeckckemhmggkdfjkjaiikadnd', // Open in Yandex (Chrome)
-    'dlobcokaeaiaihfebfhpdoongldphgkf', // Open in Yandex (Opera)
     'bffjckjhidlcnenenacdahhpbacpgapo', // Country Flags (Chrome)
-    'ljealnphmlekclcdoeficainfegfjgkc', // Country Flags (Opera)
-    'djlddpgigallklconlgapabopblddaki', // Edit with Photoshop (Chrome) [By @ray-lothian]
-    'babedbfkpnfmnopiagfocoelpfmpldkc', // Edit with Photoshop (Opera) [By @ray-lothian]
-    'kakiifnbkffafinaifemmingfmfjdklh', // Edit with Paint.Net (Chrome) [By @ray-lothian]
-    'cbdomccfnegbmmpfakfpnonihdnfmonn', // Edit with Paint.Net (Opera) [By @ray-lothian]
-    'oahnpfaakepnaabalmemoljdonbdjeei', // Easy Video Downloader (Opera) [By @ray-lothian]
     'eaicplkoeceoelookkiaeekhodehdhde', // Easy Video Downloader (Chrome) [By @ray-lothian]
-    'oofmnabdpcibefadlibdpnnbglcehfpj', // Email Client for Notmuch [By @InBasic]
-    'ghnflaojldkldmjdiocenfmeonemoogb', // Email Client for Notmuch [By @InBasic]
-    'lhplfipknbnglagbgbfogdaihdcekfga', // Open in Foxit Reader [By @lunu-bounir]
-    'dkkkgkdappindobbhmcjnjidkggbdiil', // Open in PDF-XChange Viewer [By @lunu-bounir]
     'bhfenhhfpcpkknkahnlogooiodcofkjl', // Open in Chromium (Chrome)
-    'dilpigogejfnnahbbmnpjmccammdaiom', // Open in Chromium (Opera)
     'mgmnomlncpmfgelhofilonnecmbdaoia', // Open in Brave (Chrome) [By @brian-girko]
-    'pfenoignchfmjmphnpfihgockdhejcjf', // Open in Sumatra (Chrome)
+    'oofmnabdpcibefadlibdpnnbglcehfpj', // Email Client for Notmuch (Chrome) [By @InBasic]
+    'lhplfipknbnglagbgbfogdaihdcekfga', // Open in Foxit Reader (Chrome) [By @lunu-bounir]
+
+    'hhalgjmpmjelidhhjldondajffjbcmcg', // Open in Firefox (Opera)
+    'jjlkfjglmelpbkgfeebcdipalggpapag', // Open in Edge (Opera)
+    'bdlaolidgmapdilglbocpmbfipdpfjef', // Open in Safari (Opera)
+    'noedpljikmfalclpadmnjbabbignpfge', // Open in IE (Opera)
+    'gmdcejkkcimnbfmicdpohgicekomipde', // Open in Chrome (Opera)
+    'dilpigogejfnnahbbmnpjmccammdaiom', // Open in Chromium (Opera)
+    'jfpmkcifglhenaihlnefaccjbmcmbigf', // Open in VLC (Opera)
+    'cmjahocdpafkodabbojjaebogoigcipj', // Open in GIMP (Opera)
+    'dlobcokaeaiaihfebfhpdoongldphgkf', // Open in Yandex (Opera)
+    'fabccabfpmdadbhljpcmcbmepepfllnb', // Media Converter (Opera) [By @InBasic]
+    'enemdfoackoekaedijjmjlckkleokhih', // External Application Button (Opera)
+    'iajkcajifgglcaojccgiohphgjmoleia', // Auto Shutdown (Opera)
+    'ldoihpohemnbokndocafmiblepbjeacb', // Open in Vivaldi (Opera)
+    'emlgnifoahkaeocfmafkpnfbijoioefh', // Open in Tor Browser (Opera)
+    'cglibgbhfapdcidmcpnbbekdanacjgkf', // Open in Pale Moon (Opera)
+    'hedcbilfmphgkpnokgcflaljgpecagfd', // Open in Waterfox (Opera)
+    'knfbgpkbkfebddfbklfpgmdjgolnkkfl', // Tor Control (Opera)
+    'khgbdhkpcapllhgfekjegcinegfhjbmi', // YVAD by (Opera) [By @InBasic]
+    'honagjpkinogkppkapphkpdffnegagge', // Open in PDF viewer (Opera)
+    'ljealnphmlekclcdoeficainfegfjgkc', // Country Flags (Opera)
+    'babedbfkpnfmnopiagfocoelpfmpldkc', // Edit with Photoshop (Opera) [By @ray-lothian]
+    'cbdomccfnegbmmpfakfpnonihdnfmonn', // Edit with Paint.Net (Opera) [By @ray-lothian]
     'ojmcfmboidiokgkgmilnmjnjkdbgakpn', // Open in Sumatra (Opera)
+    'ghnflaojldkldmjdiocenfmeonemoogb', // Email Client for Notmuch (Opera) [By @InBasic]
+
+    'ajgodcbbfnpdbopgmfcgdbfhabbnilbp', // Open in Firefox (Edge)
+    'eokgbaidifojhfknoiicmnffiaakelpm', // Open in Opera (Edge)
+    'ncemgeeikbbjoojolbihkpcoiomidhfi', // Open in IE (Edge)
+    'icfhhpfimihpdgglmdlnmpaadfeaacbk', // External Application Button (Edge)
+    'hjfcjapkfahlmlefedkkpbbkeddpnnlc', // Open in VLC (Edge)
+    'hkjcboammhlecfomgabecgoppnncbhjp', // Open in Chrome (Edge)
+    'ijhhpgjhnfoalaedkmgpbehokefplfni', // Tor Control (Edge)
+    'ciphgjdgpkhlngiadnpebblpcjcoabcp', // Open in PDF viewer (Edge)
   ],
   firefox: [
     '{b8fa78dd-dae1-4839-9d0e-ce5e213083ce}', // Open in GIMP
@@ -72,26 +62,25 @@ exports.ids = {
     '{3128fe8f-f039-42ea-8b12-126f78387074}', // Open in Safari
     '{8db82a75-48fd-452a-81cf-bd40b2e60dac}', // Open in Opera
     '{42dce5a2-467b-4d7b-ad11-50f4ab3d03cf}', // Open in Yandex
+    '{086f665e-6a55-4107-9147-f9a14e72b137}', // Open in Chrome
+    '{0bcb72f8-7da8-4071-905c-2de13f5aad3a}', // Open in Vivaldi
     '{15a602dd-abe0-4cff-875b-c5acd77727b6}', // Open in Pale Moon
     '{5cf4e3be-dd11-4589-befe-1b9e5037792b}', // Open in Waterfox
-    '{0ff128a1-c286-4e73-bffa-9ae879b244d5}', // Media Converter
+    '{9d3b260b-886d-4263-b9d6-81d756ee4929}', // Open in Tor Browser
+    '{4d3bd246-4326-4ec1-bb49-a27cfd57ca08}', // Open in Chromium
+    '{0ff128a1-c286-4e73-bffa-9ae879b244d5}', // Media Converter [By @InBasic]
     '{65b77238-bb05-470a-a445-ec0efe1d66c4}', // External Application Button
-    '{086f665e-6a55-4107-9147-f9a14e72b137}', // Open in Chrome
     '{6b954d17-d17c-4a19-8fe6-ee8052a562d6}', // Open in VLC
     '{9a71ec90-d0b6-44af-833f-efe418ff8454}', // Auto Shutdown
-    '{0bcb72f8-7da8-4071-905c-2de13f5aad3a}', // Open in Vivaldi
-    '{9d3b260b-886d-4263-b9d6-81d756ee4929}', // Open in Tor Browser
     '{f73df109-8fb4-453e-8373-f59e61ca4da3}', // YVAD [By @InBasic]
     '{d22a1484-dcef-44e9-ab52-80f0f4a331a3}', // Tor Control
     '{0d3afca0-aedf-491f-b0f9-9ffc22113ea8}', // Open in PDF viewer
     '{802a552e-13d1-4683-a40a-1e5325fba4bb}', // Country Flag
-    '{e127aaca-d162-4e4c-8de6-a5ee4de1a2ec}', // Edit with Photoshop [By @ray-lothian]
     '{3e8ae4b2-678d-4a63-8104-4d4d8d3b4f46}', // Edit with Paint.Net [By @ray-lothian]
     '{cd04e15e-6b23-4648-860d-0057602a5c2a}', // Easy Video Downloader [By @ray-lothian]
     '{8e409c88-e088-4ce8-8506-5a91e6c502a8}', // Email Client for Notmuch [By @InBasic]
     '{655859e0-3c86-43a1-9794-88721dacc481}', // Open in Foxit Reader [By @lunu-bounir]
     '{00186e07-f704-41ce-90aa-b09d4f49a7db}', // Open in PDF-XChange Viewer [By @lunu-bounir]
-    '{4d3bd246-4326-4ec1-bb49-a27cfd57ca08}', // Open in Chromium
     '{c88f6be2-3757-446b-be27-27eedddbcae0}', // Open in Brave [By @brian-girko]
     '{73e2414b-dc86-4e63-8ac6-231e8efe870c}', // Open in Sumatra
   ]
--- host.js
+++ host.js
@@ -20,7 +20,7 @@ let files = [];
 const sprocess = [];
 
 const config = {
-  version: '0.9.1'
+  version: '0.9.2'
 };
 // closing node when parent process is killed
 process.stdin.resume();
